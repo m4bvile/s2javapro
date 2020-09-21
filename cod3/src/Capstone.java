b3c78d90ddc29390a22bf121fdc755cc86cd56af
jay <jay>
2020-09-21 16:30:22 +1000

    my file

---
+++ cod3/src/Capstone.java
@@ -0,0 +1,11 @@
+public class Capstone {
+    public static void main(String[] args){
+    System.out.println("Hello Jay");
+    int distance = 0;
+    System.out.println("The value is "+distance);
+    distance = 40;
+    System.out.println("The value is "+distance);
+
+    }
+}
+
